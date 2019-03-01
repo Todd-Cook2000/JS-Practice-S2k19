084e1f9368d49daf1869f3fe623e0aeb44924d41
Todd-Cook2000 <tyqw2000@gmail.com>
2019-02-28 18:46:35 -0800

    Third Commit

--- Problems/problem3.js
+++ Problems/problem3.js
@@ -4,3 +4,8 @@ Using a for X in/of Y loop, iterate through the following array of objects and c
 */
 
 let antarctica = [{"country_name":"Antarctica","country_abbreviation":"AQ","capital":null},{"country_name":"Bouvet Island","country_abbreviation":"BV","capital":null},{"country_name":"French Southern territories","country_abbreviation":"TF","capital":null},{"country_name":"Heard Island and McDonald Islands","country_abbreviation":"HM","capital":null},{"country_name":"South Georgia and the South Sandwich Islands","country_abbreviation":"GS","capital":null}];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
