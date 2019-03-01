ee4c72982bc351d0041476107ca97446f5e51661
Todd-Cook2000 <tyqw2000@gmail.com>
2019-02-28 18:14:22 -0800

    First Comiit

--- Problems/problem1.js
+++ Problems/problem1.js
@@ -2,3 +2,7 @@
 Problem 1:
 Using Math, generate a random number between 0/1 and 1000 and display a whole number in the console using console.log
 */
+
+
+
+console.log(Math.floor(Math.random()* 1001));
--- Problems/problem2.js
+++ Problems/problem2.js
@@ -3,3 +3,16 @@ Problem 2:
 Declare an empty array. Using your code from problem one, populate that array with 100 random numbers.
 Output your array in JSON using JSON.stringify(your_array_here) and console.log()
 */
+
+// var numbers = [100];
+
+var arr = [100];
+for (var i=0, t=100; i<t; i++) {
+    arr.push(Math.round(Math.random() * 101))
+}
+
+console.log(arr)
+
+var myJSON = JSON.stringify(arr);
+
+console.log(myJSON);
